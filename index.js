1cc0f789a65a157931b886183843a239a75bb48a
Stephanie Coy <scoy028@gmail.com>
2018-11-04 18:20:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay(musicianArray, instrumentArray) {
+  let anArr = [];
+  for (let i = 0; i < musicianArray.length; i++) {
+    let curMus = musicianArray[i];
+    let curIns = instrumentArray[i];
+    anArr.push(curMus + " plays " + curIns);
+  }
+  return anArr;
+}
+
+function johnLennonFacts(factsArr) {
+  let anArr2 = [];
+  let i = 0;
+  while (i < factsArr.length) {
+    let curFact = factsArr[i];
+    curFact = curFact + "!!!";
+    /anArr2.push(curFact);
+  }
+  return anArr2;
+}
